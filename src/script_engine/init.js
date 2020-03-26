c4bd2b1068357720a70c01449d78a54ac0f871d7
Tglman <tglman@tglman.com>
2020-03-26 22:19:46 +0000

    add utility functions in the init script for all the methods

--- src/script_engine/init.js
+++ src/script_engine/init.js
@@ -13,3 +13,20 @@ client.global.get = function (key) {
    }
    return null;
 };
+
+function readCookie(response, variable_name) {
+	let set_cookie = response.headers['set-cookie'];
+	let pos = set_cookie.indexOf(';');
+	client.global.set(variable_name, set_cookie.substring(0,pos));
+}
+
+function assertEquals(first, second, message)  {
+	if (first != second) {
+		let gen_message = "assertion equals failed first: "+ first + " second : "+ second;
+		if (message) {
+			gen_message += message;
+		}
+		throw gen_message;
+	}
+}
+
