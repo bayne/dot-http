d1f16ca15042ec4a0cc2758503409dd7dd96b0f5
Tglman <tglman@tglman.com>
2020-04-04 13:48:30 +0100

    add checks on the scripts for avoid runtime errors

--- src/script_engine/init.js
+++ src/script_engine/init.js
@@ -16,15 +16,23 @@ client.global.get = function (key) {
 
 function readCookie(response, variable_name) {
 	let set_cookie = response.headers['set-cookie'];
-	let pos = set_cookie.indexOf(';');
-	client.global.set(variable_name, set_cookie.substring(0,pos));
+	if (set_cookie) {
+		let pos = set_cookie.indexOf(';');
+		if (pos > 0) {
+			client.global.set(variable_name, set_cookie.substring(0,pos));
+		}
+	}
 }
 
-function assertEquals(first, second, message)  {
+function assertEquals(first, second)  {
+	assertEqualsMessage(first,second,undefined);
+}
+
+function assertEqualsMessage(first, second, message)  {
 	if (first != second) {
 		let gen_message = "assertion equals failed first: "+ first + " second : "+ second;
 		if (message) {
-			gen_message += message;
+			gen_message +=" " +message;
 		}
 		throw gen_message;
 	}
