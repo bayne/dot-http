fc2d779e97c1d697f5c1c1225bb454264a7c1cc0
Tglman <tglman@tglman.com>
2020-08-26 19:11:47 +0200

    removed old print outputter

--- src/output/print.rs
+++ src/output/print.rs
@@ -1,7 +1,6 @@
 use crate::output::{prettify_response_body, FormatItem, Outputter};
 use crate::{Request, Response, Result};
 use std::io::Write;
-use std::ops::Add;
 
 pub struct FormattedOutputter<'a, W: Write> {
     writer: &'a mut W,
@@ -82,56 +81,3 @@ impl<'a, W: Write> Outputter for FormattedOutputter<'a, W> {
     }
 }
 
-pub struct PrintOutputter<'a, W: Write> {
-    writer: &'a mut W,
-}
-
-impl<'a, W: Write> PrintOutputter<'a, W> {
-    pub fn new(writer: &mut W) -> PrintOutputter<W> {
-        PrintOutputter { writer }
-    }
-}
-
-impl<'a, W: Write> Outputter for PrintOutputter<'a, W> {
-    fn response(&mut self, response: &Response) -> Result<()> {
-        let Response {
-            headers,
-            version,
-            status,
-            body,
-            ..
-        } = response;
-
-        let headers: String = headers
-            .iter()
-            .map(|(key, value)| format!("\n{}: {}", key, value))
-            .collect();
-        let response = format!(
-            "\
-{http_version} {status}\
-{headers}\
-{body}\
-            ",
-            http_version = version,
-            status = status,
-            headers = headers,
-            body = match body {
-                Some(body) => String::from("\n\n")
-                    .add(&prettify_response_body(&body))
-                    .add("\n"),
-                None => String::from(""),
-            }
-        );
-
-        self.writer.write_all(response.as_bytes())?;
-
-        Ok(())
-    }
-
-    fn request(&mut self, request: &Request) -> Result<()> {
-        let Request { method, target, .. } = request;
-        let request = format!("{method} {target}\n", method = method, target = target);
-        self.writer.write_all(request.as_bytes())?;
-        Ok(())
-    }
-}
